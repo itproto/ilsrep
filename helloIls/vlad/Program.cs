ece452075f01480f7135666d1dc5ab2203b124c0
vladykx <vladykx@gmail.com>
2008-06-09 20:54:40 +0000

    First commit

---
+++ helloIls/vlad/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloILS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("VLAD: Hello ILS!");
+        }
+    }
+}
