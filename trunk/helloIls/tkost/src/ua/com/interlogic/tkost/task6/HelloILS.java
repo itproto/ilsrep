d0083da0905e26f6963d9b3b9a41f9d862d93dd6
taraskostiak <taraskostiak@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-06-10 09:11:14 +0000

    TKOST: task 6 - initial commit

---
+++ trunk/helloIls/tkost/src/ua/com/interlogic/tkost/task6/HelloILS.java
@@ -0,0 +1,14 @@
+package ua.com.interlogic.tkost.task6;
+
+/**
+ * Task6 main class.
+ * 
+ * @author Taras Kostiak
+ * 
+ */
+public class HelloILS {
+
+    public static void main(String[] args) {
+        System.out.println("TKOST: Hello ILS!");
+    }
+}
