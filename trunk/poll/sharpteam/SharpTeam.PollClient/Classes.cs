262443bff437b6824340718d86cd36b61eaccb3c
kostjerry <kostjerry@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-06-13 20:17:21 +0000

    + Classes.cs

---
+++ trunk/poll/sharpteam/SharpTeam.PollClient/Classes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpTeam.PollClient
+{
+    public class Choice
+    {
+        public int id;
+        public string choice;
+    }
+
+    public class Poll
+    {
+        public int id;
+        public string name;
+        public string description;
+        public bool customChoice;
+        public List<Choice> choice = new List<Choice>();
+    }
+}
