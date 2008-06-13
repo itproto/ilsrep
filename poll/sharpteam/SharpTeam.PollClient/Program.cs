6c62230432bc68dfef955df1143915f538e7020f
kostjerry <kostjerry@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-06-13 20:07:50 +0000

    Parser completely fixed (without customChoiceEnabled)

--- poll/sharpteam/SharpTeam.PollClient/Program.cs
+++ poll/sharpteam/SharpTeam.PollClient/Program.cs
@@ -6,103 +6,50 @@ using System.Xml;
 
 namespace SharpTeam.PollClient
 {
-    public class Poll : ICloneable
-    {
-            public object Clone() 
-            {        
-                Poll mc = this.MemberwiseClone() as Poll;        
-                mc.id = this.id;
-                mc.name = this.name.Clone() as string;
-                mc.description = this.description.Clone() as string;
-                mc.customChoice = this.customChoice;
-                mc.choice = this.choice.Clone() as ArrayList;
-                return mc;    
-            }
-
-        public int id;
-        public string name;
-        public string description;
-        public bool customChoice;
-        public ArrayList choice = new ArrayList();
-    }
-
-    public class Choice : ICloneable
-    {
-        public object Clone()
-        {
-            Choice mc = this.MemberwiseClone() as Choice;
-            mc.id = this.id;
-            mc.choice = this.choice.Clone() as string;
-            return mc;
-        }
-
-        public int id;
-        public string choice;
-    }
-
     public class Program
     {
         static void Main(string[] args)
         {
-            int i, j, k;
-            Choice tempChoice = new Choice();
-            Poll tempPoll = new Poll();
-            ArrayList pollDoc = new ArrayList();
+            //---------------Init---------------
+            int i, j;
+            List<Poll> pollDoc = new List<Poll>();
             XmlDocument xmldoc = new XmlDocument();
             xmldoc.Load("Polls.xml");
             XmlNodeList xmlPoll = xmldoc.GetElementsByTagName("poll");
-            
+
+            //---------------Fill pollDoc---------------
             for (i = 0; i < xmlPoll.Count; i++)
             {
+                Poll currentPoll = new Poll();
                 XmlAttributeCollection xmlAttr = xmlPoll[i].Attributes;
-                for (k = 0; k < xmlAttr.Count; k++)
-                {
-                    switch (k)
-                    {
-                    case 0:
-                        tempPoll.id = Convert.ToInt32(xmlAttr[k].Value);
-                        break;
-                    case 1:
-                        tempPoll.name = xmlAttr[k].Value;
-                        break;
-                    case 2:
-                        tempPoll.customChoice = Convert.ToBoolean(xmlAttr[k].Value);
-                        break;
-                    }
-                }
-                tempPoll.description = xmlPoll[i].FirstChild.InnerText;
+                currentPoll.id = Convert.ToInt32(xmlAttr.GetNamedItem("id").Value);
+                currentPoll.name = xmlAttr.GetNamedItem("name").Value;
+                //if (xmlAttr.GetNamedItem("customChoiceEnabled"))
+                    //currentPoll.customChoice = Convert.ToBoolean(xmlAttr.GetNamedItem("customChoiceEnabled").Value);
+                currentPoll.description = xmlPoll[i].FirstChild.InnerText;
 
                 XmlNodeList xmlChoices = xmlPoll[i].ChildNodes;
                 XmlNodeList xmlChoice = xmlChoices[1].ChildNodes;
                 for (j = 0; j < xmlChoice.Count; j++)
                 {
-                    XmlAttributeCollection xmlAttrCh = xmlChoice[j].Attributes;
-                    for (k = 0; k < xmlAttr.Count; k++)
-                    {
-                        switch (k)
-                        {
-                            case 0:
-                                tempChoice.id = Convert.ToInt32(xmlAttrCh[k].Value);
-                                break;
-                            case 1:
-                                tempChoice.choice = xmlAttrCh[k].Value;
-                                break;
-                        }
-                    }
-                    tempPoll.choice.Add(tempChoice.Clone());
+                    Choice currentChoice = new Choice();
+                    XmlAttributeCollection xmlAttrChoice = xmlChoice[j].Attributes;
+                    currentChoice.id = Convert.ToInt32(xmlAttrChoice.GetNamedItem("id").Value);
+                    currentChoice.choice = xmlAttrChoice.GetNamedItem("name").Value;
+                    currentPoll.choice.Add(currentChoice);
                 }
-                pollDoc.Add(tempPoll.Clone());
-                tempPoll.choice.Clear();
+                pollDoc.Add(currentPoll);
             }
 
-            foreach(Poll myPoll in pollDoc)
+            //---------------Display contains of pollDoc---------------
+            foreach(Poll tempPoll in pollDoc)
             {
-                Console.WriteLine(myPoll.id + ") Name: " + myPoll.name);
-                Console.WriteLine("   Description: " + myPoll.description);
-                Console.WriteLine("   CustomChoiceEnabled: " + myPoll.customChoice);
-                foreach (Choice myChoice in myPoll.choice)
+                Console.WriteLine(tempPoll.id + ") Name: " + tempPoll.name);
+                Console.WriteLine("   Description: " + tempPoll.description);
+                Console.WriteLine("   CustomChoiceEnabled: " + tempPoll.customChoice);
+                foreach (Choice tempChoice in tempPoll.choice)
                 {
-                    Console.WriteLine("      " + myChoice.id + ". " + myChoice.choice);
+                    Console.WriteLine("      " + tempChoice.id + ". " + tempChoice.choice);
                 }
             }
             Console.ReadKey(true);
