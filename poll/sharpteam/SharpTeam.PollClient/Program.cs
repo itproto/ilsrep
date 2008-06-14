82a6a8bd731f264cfe2a79ae4cc693f75cf3ca6e
vladykx <vladykx@gmail.com>
2008-06-14 21:25:00 +0000

    wrote DisplayPollDoc method, which shows poll questions on the screen and afterwards displays user choices.

--- poll/sharpteam/SharpTeam.PollClient/Program.cs
+++ poll/sharpteam/SharpTeam.PollClient/Program.cs
@@ -12,14 +12,22 @@ namespace SharpTeam.PollClient
 {
     public class Program
     {
-
         public static List<Poll> ParseXml(string PATH_TO_POLLS)
         {
             //---------------Init---------------
             bool customChoiceExists;
             List<Poll> pollDoc = new List<Poll>();
             XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(PATH_TO_POLLS);
+            try
+            {
+                xmlDoc.Load(PATH_TO_POLLS);
+            }
+            catch(Exception)
+            {
+                System.Console.WriteLine("Couldn't find xml file: " + PATH_TO_POLLS);
+                Environment.Exit(-1);
+            }
+            
             XmlNodeList xmlPollList = xmlDoc.GetElementsByTagName("poll");
 
             //---------------Fill pollDoc---------------
@@ -51,15 +59,81 @@ namespace SharpTeam.PollClient
 
         public static void DisplayPollDoc(List<Poll> pollDoc)
         {
+            List<Choice> userChoices = new List<Choice>();
+
             foreach (Poll curPoll in pollDoc)
             {
-                Console.WriteLine(curPoll.id + ") Name: " + curPoll.name);
-                Console.WriteLine("   Description: " + curPoll.description);
-                Console.WriteLine("   CustomChoiceEnabled: " + curPoll.customChoice);
+                // Clear screen and write this poll's question
+                Console.Clear();
+                Console.WriteLine(curPoll.name + "\n" + curPoll.description);
+                
+                // List choices for this poll
+                int index = 1;
                 foreach (Choice curChoice in curPoll.choice)
                 {
-                    Console.WriteLine("      " + curChoice.id + ". " + curChoice.choice);
+                    Console.WriteLine("\t" + index + ". " + curChoice.choice);
+                    ++ index;
                 }
+
+                // add option for customer choice
+                if ( curPoll.customChoice == true )
+                {
+                    Console.WriteLine("\t" + index + ". Custom Choice");
+                }
+
+                Console.Write("Pick your choice: [1-" + (curPoll.customChoice ? curPoll.choice.Count + 1 : curPoll.choice.Count) + "]:");
+                
+                // accept only correct choices
+                while ( true )
+                {
+                    // Get user choice but don't show ( not sure if it's correct )
+                    try
+                    {
+                        index = int.Parse(Console.ReadKey(true).KeyChar.ToString());
+                        --index;
+                    }
+                    catch( Exception )
+                    {
+                        continue;
+                    }
+                    
+                    if ( index > 0 && index <= curPoll.choice.Count - ( curPoll.customChoice ? 0 : 1 ) )
+                    {
+                        // show user choice
+                        break;
+                    } 
+                }
+
+                // check if custom choice
+                if ( index == curPoll.choice.Count )
+                {
+                    Console.Write("\nEnter your choice:" );
+
+                    // create custom choice
+                    Choice userChoice = new Choice();
+                    userChoice.choice = Console.ReadLine();
+                    userChoice.id = 0;
+
+                    // add custom choice to list
+                    userChoices.Add(userChoice);
+                }
+                else
+                {
+                    // add one of the choices that already exist to list
+                    userChoices.Add(curPoll.choice[index]);
+                }
+
+            // end of loop of polls
+            }
+
+            // list choices
+            Console.Clear();
+            Console.WriteLine("Your choices:");
+
+            // don't know how to do this with foreach... ((
+            for (int i = 0; i < pollDoc.Count; ++ i )
+            {
+                Console.WriteLine(pollDoc[i].name + ": " + (userChoices[i].id == 0 ? "Custom Choice: " : userChoices[i].id + ". ") + userChoices[i].choice);
             }
         }
 
@@ -68,6 +142,8 @@ namespace SharpTeam.PollClient
             const string PATH_TO_POLLS = "Polls.xml";
             List<Poll> pollDoc = ParseXml(PATH_TO_POLLS);
             DisplayPollDoc(pollDoc);
+
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey(true);
         }
     }
