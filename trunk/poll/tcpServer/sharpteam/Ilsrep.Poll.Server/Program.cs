f0913dcf3c6824fc847284f5e9c4aa7ba1231fa7
vladykx@gmail.com <vladykx@gmail.com@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-06-19 17:36:06 +0000

    - added Polls.xml - added methods SendData and readFile

--- trunk/poll/tcpServer/sharpteam/Ilsrep.Poll.Server/Program.cs
+++ trunk/poll/tcpServer/sharpteam/Ilsrep.Poll.Server/Program.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Xml;
+using System.IO;
 
 namespace Ilsrep.Poll.Server
 {
@@ -10,6 +11,7 @@ namespace Ilsrep.Poll.Server
     {
         private const int PORT = 3320;
         private const int maxPendingConnCount = 10;
+        private const string POLL_FILE = "Polls.xml";
 
         public static int GetId(string xmlStringId)
         {
@@ -19,9 +21,34 @@ namespace Ilsrep.Poll.Server
             return Convert.ToInt32(pollSessionId[0].InnerText);
         }
 
+        public static string readFile(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            String fileData = sr.ReadToEnd();
+            sr.Close();
+
+            return fileData;
+        }
+
+        private static int SendData(Socket s, byte[] data)
+        {
+            int total = 0;
+            int size = data.Length;
+            int dataleft = size;
+            int sent;
+
+            while (total < size)
+            {
+                sent = s.Send(data, total, dataleft, SocketFlags.None);
+                total += sent;
+                dataleft -= sent;
+            }
+            return total;
+        }
+
         public static void Main()
         {
-            byte[] data = new byte[1024];
+            Byte[] data = new Byte[1024];
             IPEndPoint clientAddress = new IPEndPoint(IPAddress.Any, PORT);
 
             Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -38,16 +65,23 @@ namespace Ilsrep.Poll.Server
             //client.Send(data, data.Length, SocketFlags.None);
             // ------------------------------------------------------------ //
 
-            int c = client.Receive(data);
-            string receicedData = Encoding.ASCII.GetString(data);
-            Console.WriteLine(c + "\t" + receicedData);
+            //int c = client.Receive(data);
+            //string receicedData = Encoding.ASCII.GetString(data);
+            //Console.WriteLine(c + "\t" + receicedData);
             //int id = GetId(receicedData);
             //Console.WriteLine("Received id: " + id);
 
-            Console.ReadKey(true);
-            Console.WriteLine("Disconnected from {0}", clientInfo.Address);
+            //Console.ReadKey(true);
+            String fileData = readFile(POLL_FILE);
+            data = Encoding.ASCII.GetBytes(fileData);
+
+            int countSent = SendData(client, data);
+            Console.WriteLine("Sent to {0} {1} bytes", clientInfo.Address, countSent);
+
             client.Close();
+            Console.WriteLine("Disconnected from {0}", clientInfo.Address);
             Console.ReadKey(true);
+
         }
     }
 }
