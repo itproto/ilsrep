ab36044a16fdb202f7059adc7e9d3c4d0a4ebc72
taraskostiak <taraskostiak@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-06-30 23:31:37 +0000

    task 8(poll server) - poll server initial commit.

--- poll/jteam/src/ilsrep/poll/common/Pollsession.java
+++ poll/jteam/src/ilsrep/poll/common/Pollsession.java
@@ -13,46 +13,80 @@ import javax.xml.bind.annotation.XmlAttribute;
  * 
  */
 @XmlRootElement(name = "pollsession")
-
 public class Pollsession {
-private String minScore="-1", name,id,testMode="false";
+
+    /**
+     * TODO: DRC: Separate: each variable - its line; all are initialised with
+     * <code>null</code>(fix logic where it interfere); each variable - must
+     * have comment.
+     */
+    private String minScore = "-1", name, id, testMode = "false";
+
     /**
      * "poll" elements.
      */
     protected List<Poll> polls = null;
 
     /**
-     * @see #polls
+     * @see #minScore
      */
-      @XmlAttribute(name = "minScore")
+    @XmlAttribute(name = "minScore")
     public String getMinScore() {
         return minScore;
     }
+
     public void setMinScore(String minScore) {
-        this.minScore =minScore;
-    }
-    @XmlAttribute(name = "name")
-   public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name =name;
-    }
-    @XmlAttribute(name = "id")
-   public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id =id;
-    }
-    @XmlAttribute(name = "testMode")
-   public String getTestMode() {
-        return testMode;
-    }
-    public void setTestMode(String testMode) {
-        this.testMode =testMode;
+        this.minScore = minScore;
     }
 
+    /**
+     * @see #name
+     */
+    @XmlAttribute(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @see #name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @see #id
+     */
+    @XmlAttribute(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @see #id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @see #testMode
+     */
+    @XmlAttribute(name = "testMode")
+    public String getTestMode() {
+        return testMode;
+    }
+
+    /**
+     * @see #testMode
+     */
+    public void setTestMode(String testMode) {
+        this.testMode = testMode;
+    }
+
+    /**
+     * @see #polls
+     */
     @XmlElementRef
     public List<Poll> getPolls() {
         return polls;
@@ -65,5 +99,4 @@ private String minScore="-1", name,id,testMode="false";
         this.polls = polls;
     }
 
-    
 }
