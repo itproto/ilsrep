7c064eace1b97b983b9d90d25b6cfcdcf01d7ba6
taraskostiak <taraskostiak@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-07-03 19:34:04 +0000

    task 8(poll server): - server skeleton - xpilotpanel-lib.jar update(bug with not translated "Save" and "Cancel" buttons fixed)

--- poll/jteam/src/ilsrep/poll/server/ConfigurationEditor.java
+++ poll/jteam/src/ilsrep/poll/server/ConfigurationEditor.java
@@ -64,38 +64,30 @@ public class ConfigurationEditor {
      * @param configurationFile
      */
     public static void editConfiguration(File configurationFile) {
-        // Loading configuration model.
-        PreferencesModel configurationModel = null;
-        try {
-            configurationModel = PreferencesModel
-                    .loadModelFromURL(ConfigurationEditor.class
-                            .getClassLoader().getResource(
-                                    "data/configurationModel.xml"));
-        }
-        // Is thrown when configuration file can't be loaded.
-        catch (IOException e) {
-            System.out
-                    .println("ERROR: while loading configuration model. Quitting!");
-            System.exit(1);
-        }
-        // Is thrown when configuration file is corrupted(JAXB can't parse xml).
-        catch (JAXBException e) {
-            System.out
-                    .println("ERROR: configuration model is corrupted. Quitting!");
-            System.exit(2);
-        }
-
+        // Loading Preferences using model in jar and specified file.
         Preferences configuration = null;
         try {
-            configuration = Preferences.loadPreferences(configurationModel,
-                    new FileInputStream(configurationFile));
+            configuration = loadPreferences(configurationFile);
         }
         catch (FileNotFoundException e) {
             System.out
                     .println("ERROR: File was deleted by other program after selected. Quitting");
             System.exit(3);
         }
+        catch (IOException e) { // Is thrown when configuration file can't be
+            // loaded.
+            System.out
+                    .println("ERROR: while loading configuration model. Quitting!");
+            System.exit(1);
+        }
+        catch (JAXBException e) { // Is thrown when configuration file is
+            // corrupted(JAXB can't parse xml).
+            System.out
+                    .println("ERROR: configuration model is corrupted. Quitting!");
+            System.exit(2);
+        }
 
+        // Building and starting PreferencesEditor.
         XPPI18N i18nSystem = new XPPI18N("data");
         PreferencesEditor configurationEditor = new PreferencesEditor(
                 configuration, i18nSystem, "i18n");
@@ -103,11 +95,14 @@ public class ConfigurationEditor {
         configurationEditor.setVisible(true);
 
         try {
-            Thread.sleep(0);
+            Thread.sleep(0); // Making current thread wait till
+            // PreferencesEditor finishes editing and interrupt this thread to
+            // execute code below.
         }
-        catch (InterruptedException e1) {
+        catch (InterruptedException e) {
         }
 
+        // Saving Preferences to same file from which they were loaded.
         OutputStream savingStream = null;
         try {
             savingStream = new FileOutputStream(configurationFile);
@@ -125,5 +120,22 @@ public class ConfigurationEditor {
                             + "it). Quitting");
             System.exit(4);
         }
+    }
+
+    public static Preferences loadPreferences(File configurationFile)
+            throws IOException, JAXBException {
+        // Loading configuration model from jar.
+        PreferencesModel configurationModel = null;
+        configurationModel = PreferencesModel
+                .loadModelFromURL(ConfigurationEditor.class.getClassLoader()
+                        .getResource("data/configurationModel.xml"));
+
+        Preferences configuration = null;
+
+        // Loading preferences from specified file.
+        configuration = Preferences.loadPreferences(configurationModel,
+                new FileInputStream(configurationFile));
+
+        return configuration;
     }
 }
