d189b31060660a5b3ed3116918d1bae4748c9ff2
vladykx <vladykx@gmail.com>
2008-07-07 10:45:37 +0000

    - Renaming to - Renaming from SharpTeam.PollClient to Ilsrep.PollApplication.PollClient to match the rest.

---
+++ poll/sharpteam/Ilsrep.PollApplication.PollClient/Model/Choice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Ilsrep.PollApplication.Model
+{
+    [XmlRoot("choice"), Serializable]
+    public class Choice
+    {
+        [XmlAttribute("id")] public int id;
+        [XmlAttribute("name")] public string choice;
+        [XmlIgnore] public Poll parent;
+    }
+}
