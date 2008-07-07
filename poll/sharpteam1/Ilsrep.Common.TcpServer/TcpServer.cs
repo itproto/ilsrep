ec88bd3442626cfdc019cb2667302a02bfdac88f
vladykx <vladykx@gmail.com>
2008-07-07 14:50:34 +0000

    - directory change

---
+++ poll/sharpteam1/Ilsrep.Common.TcpServer/TcpServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Ilsrep.Common.TcpServer
+{
+    public class TcpServer
+    {
+        Socket m_client;
+
+        public bool isConnected
+        {
+            get 
+            { 
+                return m_client.Connected; 
+            }
+        }
+
+        public TcpServer()
+        {
+            m_client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Connect(string address, int port)
+        {
+            m_client.Connect(address, port);
+        }
+
+        public void Disconnect()
+        {
+            m_client.Disconnect(false);
+        }
+
+        public void Send(string data)
+        {
+            m_client.Send(Encoding.ASCII.GetBytes(data));
+        }
+
+        public string Receive()
+        {
+            Byte[] receivedData = new Byte[1024];
+
+            m_client.Receive(receivedData);
+
+            return Encoding.ASCII.GetString(receivedData);
+        }
+    }
+}
---
+++ poll/sharpteam1/Ilsrep.PollApplication.Protocol/Helpers/CommandLineParametersHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace Ilsrep.PollApplication.Helpers
+{
+    class CommandLineParametersHelper
+    {
+        public static NameValueCollection Parse(string[] args)
+        {
+            String key = String.Empty;
+            NameValueCollection commandlineParameters = new NameValueCollection();
+
+            foreach (String commandlineParameter in args)
+            {
+                if (key == String.Empty && commandlineParameter.Substring(0, 1) == "-")
+                {
+                    key = commandlineParameter.Substring(1);
+                }
+                else if (key != String.Empty)
+                {
+                    commandlineParameters.Add(key, commandlineParameter);
+                    key = String.Empty;
+                }
+            }
+
+            return commandlineParameters;
+        }
+    }
+}
