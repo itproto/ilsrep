1f75e6878b634fa20dfad22ebc9bde20b9460745
taraskostiak <taraskostiak@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-07-16 12:01:03 +0000

    task 7/8(poll client/server) - xml protocol classes and their tester(ProtocolTester) added

--- poll/jteam/src/ilsrep/poll/common/Pollsession.java
+++ poll/jteam/src/ilsrep/poll/common/Pollsession.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlAttribute;
 
 /**
- * The "polls" element.
+ * The "pollsession" element.
  * 
  * @author TKOST
  * 
