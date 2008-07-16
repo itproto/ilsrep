a50fcf42f71f8c41b329a4b00eae8266a9d188da
taraskostiak <taraskostiak@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-07-16 12:03:14 +0000

    task 7/8(poll client/server) - rev190 missing files added (xml protocol classes and their tester(ProtocolTester) added)

---
+++ trunk/poll/jteam/src/ilsrep/poll/common/Pollpacket.java
@@ -0,0 +1,76 @@
+package ilsrep.poll.common;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * The "pollpacket" element.
+ * 
+ * @author TKOST
+ * 
+ */
+@XmlRootElement(name = "pollpacket")
+public class Pollpacket {
+
+    /**
+     * Client's request to server.
+     */
+    protected Request request = null;
+
+    /**
+     * Server's response for client's request of pollsession list stored on
+     * server.
+     */
+    protected Pollsessionlist pollsessionList = null;
+
+    /**
+     * Used when:
+     * <ul>
+     * <li>server sends poll xml for client's request</li>
+     * <li>editor sends xml created by him to server</li>
+     * </ul>
+     */
+    protected Pollsession pollsession = null;
+
+    /**
+     * @see #request
+     */
+    public Request getRequest() {
+        return request;
+    }
+
+    /**
+     * @see #request
+     */
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    /**
+     * @see #pollsessionList
+     */
+    public Pollsessionlist getPollsessionList() {
+        return pollsessionList;
+    }
+
+    /**
+     * @see #pollsessionList
+     */
+    public void setPollsessionList(Pollsessionlist pollsessionList) {
+        this.pollsessionList = pollsessionList;
+    }
+
+    /**
+     * @see #pollsession
+     */
+    public Pollsession getPollsession() {
+        return pollsession;
+    }
+
+    /**
+     * @see #pollsession
+     */
+    public void setPollsession(Pollsession pollsession) {
+        this.pollsession = pollsession;
+    }
+
+}
