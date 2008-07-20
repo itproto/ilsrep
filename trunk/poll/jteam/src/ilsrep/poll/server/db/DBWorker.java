8c194b69b1167f366910fdf3c81c72229559be65
taraskostiak <taraskostiak@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-07-20 12:09:39 +0000

    task 7/8(poll client/server) - DB basic operations work(not implemented into server logic yet). used pool of connections to DB

--- trunk/poll/jteam/src/ilsrep/poll/server/db/DBWorker.java
+++ trunk/poll/jteam/src/ilsrep/poll/server/db/DBWorker.java
@@ -1,50 +1,93 @@
 package ilsrep.poll.server.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
 import ilsrep.poll.common.Pollsessionlist;
 import ilsrep.poll.common.Item;
-import java.sql.*;
-import java.util.*;
 
 /**
  * This abstract class is utility for working with any DB.<br>
  * There should be concrete class, which works with concrete DB.
  * 
  * @author TKOST
+ * @author DRC
  * 
  */
 public abstract class DBWorker {
-public Connection conn;
+
     /**
-     * Establishes connection to concrete(should be overriden) DB.
+     * <code>DataSource</code> for DB.<br>
+     * Usually a pool.
+     */
+    protected DataSource dataSource = null;
+
+    /**
+     * DB's URL for JDBC.
+     */
+    protected String dbURL = null;
+
+    /**
+     * DB driver's class name.
+     */
+    protected String driverClassName = null;
+
+    /**
+     * Initialises connection pool to concrete(should be overridden) DB.
      * 
      * @throws SQLException
      *             On some DB problems.
      */
-    public abstract void connect() throws SQLException;
+    public void connect() throws SQLException {
+        if (dataSource == null) {
+            BasicDataSource dbPool = new BasicDataSource();
+            dbPool.setDriverClassName(driverClassName);
+            dbPool.setUrl(dbURL);
+
+            dataSource = dbPool;
+
+            // TODO: Set pool's options.
+        }
+    }
 
     /**
      * Fetches pollsession from DB by id.
      * 
      * @param id
      *            Id of pollsession to fetch.
-     * @return Pollsession as string.
+     * @return Pollsession as string or null if pollsession not found.
      * @throws SQLException
      *             On some DB problems.
      */
-    public  String getPollsessionById(String id) throws SQLException{
-	    this.connect();
-	    String xmlItself;
-	   Statement stat = this.conn.createStatement(); 
-	   ResultSet rs = stat.executeQuery("select xml from polls where id="+id);
-	   if (rs.next()) {xmlItself=rs.getString("xml") ;
-	   }   else {
-		   xmlItself=null; }
-		   this.conn.close();
-		   return xmlItself;
-	    
-	    
-	    
-	    };
+    public String getPollsessionById(String id) throws SQLException {
+        connect(); // If connection isn't established yet this connects to DB.
+
+        Connection conn = dataSource.getConnection();
+        Statement stat = conn.createStatement();
+
+        String xmlItself;
+        ResultSet rs = stat
+                .executeQuery("select xml from polls where id=" + id);
+        if (rs.next()) {
+            xmlItself = rs.getString("xml");
+        }
+        else {
+            xmlItself = null;
+        }
+
+        stat.close();
+        conn.close();
+
+        return xmlItself;
+    }
 
     /**
      * Fetches pollsession list from DB.
@@ -53,26 +96,27 @@ public Connection conn;
      * @throws SQLException
      *             On some DB problems.
      */
-    public Pollsessionlist  getPollsessionlist() throws SQLException{
-	    this.connect();
-	        Pollsessionlist  pollList=new Pollsessionlist();
-	        List<Item> lstItems = new ArrayList<Item>();
-Item itm = new Item();
-	   Statement stat = this.conn.createStatement(); 
-	   ResultSet rs = stat.executeQuery("select id, name from polls");
-	   while (rs.next()) {
-		   itm.setId(Integer.toString(rs.getInt("id")));
-		   itm.setName(rs.getString("name"));
-		   lstItems.add(itm);
-		   	   }   ;
-		   	   pollList.setItems(lstItems);
-	   this.conn.close();
-	    return pollList;
-	    
-	    
-	    };
-	    
-	    
-	    
-	    };
+    public Pollsessionlist getPollsessionlist() throws SQLException {
+        connect(); // If connection isn't established yet this connects to DB.
 
+        Connection conn = dataSource.getConnection();
+        Statement stat = conn.createStatement();
+
+        Pollsessionlist pollList = new Pollsessionlist();
+        List<Item> lstItems = new ArrayList<Item>();
+        ResultSet rs = stat.executeQuery("select id, name from polls");
+        while (rs.next()) {
+            Item itm = new Item();
+            itm.setId(Integer.toString(rs.getInt("id")));
+            itm.setName(rs.getString("name"));
+            lstItems.add(itm);
+        }
+        pollList.setItems(lstItems);
+
+        stat.close();
+        conn.close();
+
+        return pollList;
+    }
+
+}
