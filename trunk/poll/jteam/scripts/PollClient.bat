69657ce5082f5c7d43354c7163add03c614d410f
draconyster <draconyster@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-08-11 09:37:26 +0000

    added clearscreen to bat files

--- trunk/poll/jteam/scripts/PollClient.bat
+++ trunk/poll/jteam/scripts/PollClient.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
+cls
 echo Would you like to take a poll or create a new one?
 echo 1) Start Client
 echo 2) Start Editor
 CHOICE /N /C:12 /M "Enter choice (1 or 2):" %1
+cls
 IF ERRORLEVEL ==2 GOTO TWO
 IF ERRORLEVEL ==1 GOTO ONE
 :TWO
