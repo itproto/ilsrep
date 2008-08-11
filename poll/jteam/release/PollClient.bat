5fd1738dee0475ce1e374ffca7b4d2b7fa8468eb
draconyster <draconyster@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-08-11 09:30:48 +0000

    Replaced 2 bat files with one general. Now start with PollClient.bat

---
+++ poll/jteam/release/PollClient.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+echo Would you like to take a poll or create a new one?
+echo 1) Start Client
+echo 2) Start Editor
+CHOICE /N /C:12 /M "Enter choice (1 or 2):" %1
+IF ERRORLEVEL ==2 GOTO TWO
+IF ERRORLEVEL ==1 GOTO ONE
+:TWO
+java -cp poll.jar;lib\xpilotpanel-lib.jar;lib\log4j-1.2.15.jar ilsrep.poll.client.PollEditor
+GOTO END
+:ONE
+java -cp poll.jar;lib\xpilotpanel-lib.jar;lib\log4j-1.2.15.jar ilsrep.poll.client.PollClient
+:END
