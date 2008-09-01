9a3349c2f530452a67a76d7b3328cd6df20b636d
kostjerry <kostjerry@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-09-01 16:43:23 +0000

    ADEED: StatisticForm ADDED: new request type in Request class: GET_RESULTS = "getResults" UPDATED: PollStatisticsForm(refresh pollSessionsListBox on load)

--- poll/sharpteam/Ilsrep.PollApplication.PollClientGUI/PollStatisticsForm.Designer.cs
+++ poll/sharpteam/Ilsrep.PollApplication.PollClientGUI/PollStatisticsForm.Designer.cs
@@ -28,19 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pollSessionsListBox = new System.Windows.Forms.ListBox();
+            this.viewStatisticsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // PollStatistics
+            // pollSessionsListBox
+            // 
+            this.pollSessionsListBox.FormattingEnabled = true;
+            this.pollSessionsListBox.HorizontalScrollbar = true;
+            this.pollSessionsListBox.Location = new System.Drawing.Point(12, 12);
+            this.pollSessionsListBox.Name = "pollSessionsListBox";
+            this.pollSessionsListBox.Size = new System.Drawing.Size(268, 108);
+            this.pollSessionsListBox.TabIndex = 0;
+            this.pollSessionsListBox.SelectedIndexChanged += new System.EventHandler(this.pollSessionsListBox_SelectedIndexChanged);
+            // 
+            // viewStatisticsButton
+            // 
+            this.viewStatisticsButton.Location = new System.Drawing.Point(12, 126);
+            this.viewStatisticsButton.Name = "viewStatisticsButton";
+            this.viewStatisticsButton.Size = new System.Drawing.Size(268, 23);
+            this.viewStatisticsButton.TabIndex = 1;
+            this.viewStatisticsButton.Text = "View statistics";
+            this.viewStatisticsButton.UseVisualStyleBackColor = true;
+            this.viewStatisticsButton.Click += new System.EventHandler(this.viewStatisticsButton_Click);
+            // 
+            // PollStatisticsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 124);
-            this.Name = "PollStatistics";
+            this.ClientSize = new System.Drawing.Size(292, 161);
+            this.Controls.Add(this.viewStatisticsButton);
+            this.Controls.Add(this.pollSessionsListBox);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 195);
+            this.MinimumSize = new System.Drawing.Size(300, 195);
+            this.Name = "PollStatisticsForm";
             this.Text = "PollStatistics";
+            this.Load += new System.EventHandler(this.PollStatisticsForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox pollSessionsListBox;
+        private System.Windows.Forms.Button viewStatisticsButton;
     }
 }
---
+++ poll/sharpteam/Ilsrep.PollApplication.PollClientGUI/StatisticForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Ilsrep.PollApplication.Communication;
+using Ilsrep.PollApplication.Helpers;
+using Ilsrep.PollApplication.Model;
+
+namespace Ilsrep.PollApplication.PollClientGUI
+{
+    /// <summary>
+    /// Statistic Form
+    /// </summary>
+    public partial class StatisticForm : Form
+    {
+        /// <summary>
+        /// Initialize Form
+        /// </summary>
+        public StatisticForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Function sends request, receive PollPacket and check if receivedPacket == null. If true, user can retry to receive Packet, else function returns receivedPacket
+        /// </summary>
+        /// <param name="sendPacket">PollPacket with request to send</param>
+        /// <returns>PollPacket receivedPacket</returns>
+        private PollPacket ReceivePollPacket(PollPacket sendPacket)
+        {
+            try
+            {
+                string sendString = PollSerializator.SerializePacket(sendPacket);
+                PollClientGUI.client.Send(sendString);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+            }
+
+            string receivedString = PollClientGUI.client.Receive();
+            PollPacket receivedPacket = new PollPacket();
+            receivedPacket = PollSerializator.DeserializePacket(receivedString);
+
+            // Check if received data is correct
+            if (receivedPacket == null)
+            {
+                MessageBox.Show("Wrong data received", "Error");
+            }
+            return receivedPacket;
+        }
+
+        /// <summary>
+        /// Get top 10 results of selected PollSession
+        /// </summary>
+        /// <param name="sender">object sender</param>
+        /// <param name="e">EventArgs e</param>
+        private void StatisticForm_Load(object sender, EventArgs e)
+        {
+            PollPacket sendPacket = new PollPacket();
+            PollPacket receivedPacket = new PollPacket();
+            sendPacket.request = new Request();
+
+            sendPacket.request.type = Request.GET_RESULTS;
+            sendPacket.request.id = PollStatisticsForm.selectedPollSession.id.ToString();
+            receivedPacket = ReceivePollPacket(sendPacket);
+            //...
+        }
+
+        /// <summary>
+        /// Close Form
+        /// </summary>
+        /// <param name="sender">object sender</param>
+        /// <param name="e">EventArgs e</param>
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
