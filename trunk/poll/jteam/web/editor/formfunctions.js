37c09dcf39e80345b140bdebe22f28e5ccb03930
draconyster <draconyster@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-10-13 18:29:12 +0000

    1) started working on polleditor 2) polleditor uses javascript + "Xml for<Script>" framework (will redo in plain forms later) Current state: the xml file is loaded into the page, and processed by javascript. as for now you can only browse the session, editing is not possible yet.

---
+++ trunk/poll/jteam/web/editor/formfunctions.js
@@ -0,0 +1,156 @@
+var docRef = document.frmEdit;
+var gEditStatus = "";
+var gCurrentPoll;
+var gobjDatabaseDom;
+var gobjDatabaseDomTree;
+var sess;
+function navigateUserList(direction) {
+        switch (direction) {
+        case "next":
+            gCurrentPoll = gCurrentPoll.getNextSibling();
+            break;
+
+        case "previous":
+            gCurrentPoll = gCurrentPoll.getPreviousSibling();
+            break;
+
+        case "first":
+            contactNodeSet = gobjDatabaseDomTree.getChildNodes();
+            gCurrentPoll = contactNodeSet.item(0);
+            break;
+
+        case "last":
+            gCurrentPoll = contactNodeSet.item(contactNodeSet.getLength() -1);
+            break;
+
+    } // end switch
+
+
+    if (gCurrentPoll != null ) {
+        displayUserData(gCurrentPoll);
+    }
+
+    //decide what to do with the navigation forms
+    setNavigationButtonStateForEOFandBOF();
+}
+function displayUserData(user) {
+	clearform();
+sess=user.getParentNode();
+	 docRef.SessName.value=sess.getAttribute("name");
+	  if(sess.getAttribute("testMode")=="true") {
+	    docRef.TestMode.checked=true;
+    } else {
+	    docRef.TestMode.checked=false;
+	    }
+    docRef.PollName.value = user.getAttribute("name");
+     docRef.PollDesc.value = user.getElementsByTagName("description").item(0).getFirstChild().getNodeValue();
+    if(user.getAttribute("customChoiceEnabled")=="true") {
+	    docRef.Custom.checked=true;
+    } else {
+	    docRef.Custom.checked=false;
+	    }
+    var length_ch = user.getElementsByTagName("choice").length;
+    for (i=0;i<length_ch;i++){
+	     var name_ch = user.getElementsByTagName("choice").item(i).getAttribute("name");
+	    if(sess.getAttribute("testMode")=="true") {
+var a=user.getElementsByTagName("choice").item(i).getAttribute("id")*1;
+var b=user.getAttribute("correctChoice")*1;
+		       if(a==b) {
+			       addRowToTable(name_ch,true);
+	     } else { 
+		     addRowToTable(name_ch,false);
+	     }
+ }   
+  else { 
+		     addRowToTable(name_ch,false);
+	     } 
+	     
+	    }
+
+
+} // end function displayUserData
+function formInit() {
+    //first set up the database object. In this test case, I know I have data,
+    var parser = new DOMImplementation();
+    gobjDatabaseDom = parser.loadXML(docRef.txtDatabase.value);
+    gobjDatabaseDomTree = gobjDatabaseDom.getDocumentElement();
+       document.getElementById("cmdAddNew").disabled = false;
+    document.getElementById("cmdEdit").disabled = false;
+    document.getElementById("cmdDelete").disabled = false;
+   navigateUserList("first");
+    // enable the add new button
+ 
+
+} 
+function addRowToTable(name_ch,checked_is)
+{
+  var tbl = document.getElementById('polltbl');
+  var lastRow = tbl.rows.length;
+  // if there's no header row in the table, then iteration = lastRow + 1
+  var iteration = lastRow-3;
+  var row = tbl.insertRow(lastRow);
+  
+  // left cell
+  var cellLeft = row.insertCell(0);
+  var el = document.createElement('input');
+  el.type = 'radio';
+  el.checked=checked_is;
+  el.name = 'pollchoices';
+  el.id = 'choiceRow' + iteration;
+  el.disabled=true;
+  el.value=name_ch;
+  el.size = 40;
+  cellLeft.appendChild(el);
+  
+  
+  // right cell
+  var cellRight = row.insertCell(1);
+  el = document.createElement('input');
+  el.type = 'text';
+  el.name = 'choice' + iteration;
+  el.id = 'choiceRow' + iteration;
+  el.disabled=true;
+  el.value=name_ch;
+  el.size = 40;
+  cellRight.appendChild(el);
+ 
+}
+function clearform() {
+    document.getElementById("PollName").value = "";
+    document.getElementById("PollDesc").value = "";
+   docRef.Custom.checked=false;
+var lgh=document.getElementById('polltbl').rows.length-3;
+ for(i=0;i<lgh;i++){
+	 	 	 RemoveRow();
+	 	 		 	 	
+	 }
+
+} 
+function setNavigationButtonStateForEOFandBOF() {
+    if (!gCurrentPoll.getPreviousSibling()) {
+        //we're on the first record
+        document.getElementById("cmdMoveFirst").disabled = true;
+        document.getElementById("cmdMovePrevious").disabled = true;
+    }
+    else {
+       document.getElementById("cmdMoveFirst").disabled = false;
+        document.getElementById("cmdMovePrevious").disabled = false;
+    }
+
+
+    if (!gCurrentPoll.getNextSibling()) {
+        //we're on the last record
+       document.getElementById("cmdMoveLast").disabled = true;
+       document.getElementById("cmdMoveNext").disabled = true;
+    }
+    else {
+       document.getElementById("cmdMoveLast").disabled = false;
+       document.getElementById("cmdMoveNext").disabled = false;
+    }
+
+} 
+function RemoveRow(){
+ obj=document.getElementById("polltbl");
+obj.deleteRow(obj.rows.length-1);
+}
+formInit();
---
+++ trunk/poll/jteam/web/editor/poll.jsp
@@ -0,0 +1,61 @@
+<%@page import="ilsrep.poll.common.model.Poll"%>
+<%@page import="ilsrep.poll.common.model.Choice"%>
+<%@page import="ilsrep.poll.common.model.Pollsession"%>
+<%@page import="ilsrep.poll.server.db.SQLiteDBManager"%>
+<%@page import="ilsrep.poll.server.db.DBManager"%>
+<%@page import="ilsrep.poll.common.protocol.AnswerItem"%>
+<%@page import="ilsrep.poll.common.protocol.Answers"%>
+<%@page import="javax.xml.bind.JAXBContext"%>
+<%@page import="javax.xml.bind.JAXBContext"%>
+<%@page import="javax.xml.bind.JAXBException"%>
+<%@page import="javax.xml.bind.Marshaller"%>
+<%@page import="java.io.StringWriter"%>
+<% String PLEASE_ENTER_POLL="Please choose poll";
+%>
+<%!
+public String editPoll(String sessi) throws Exception{
+	Pollsession sess;
+DBManager db;
+int numberOfPolls=0;
+db = new SQLiteDBManager(null,getServletContext().getRealPath("/")+"/pollserver.db");
+sess=db.getPollsessionById(sessi);
+JAXBContext pollContext = JAXBContext.newInstance(Pollsession.class);
+        Marshaller mr = pollContext.createMarshaller();
+        StringWriter wr = new StringWriter();
+        mr.marshal(sess, wr);
+        String output=wr.toString();
+        output=output.replaceAll("<","&lt;");
+        output=output.replaceAll(">","&gt;");
+	
+        output="<form name=\"frmEdit\" id=\"frmEdit\" method=\"get\" action=\"noaction\">"+
+	"<table>"+
+	"<tr><td>Session Name:</td><td><input type=\"text\" name=\"SessName\" id=\"SessName\" disabled></td></tr>"+
+	"<tr><td>Allow Test Mode</td><td><INPUT TYPE=\"checkbox\" NAME=\"TestMode\" disabled></td></tr>"+
+	"</table>"+
+	"<table name=\"polltbl\" id=\"polltbl\">"+
+	"<tr><td>Poll Name:</td><td><input type=\"text\" name=\"PollName\" id=\"PollName\" disabled></td></tr>"+
+	"<tr><td>Poll Desc:</td><td><input type=\"text\" name=\"PollDesc\" id=\"PollDesc\" disabled></td></tr>"+
+	"<tr><td>Allow custom choice</td><td><INPUT TYPE=\"checkbox\" NAME=\"Custom\" disabled></td></tr>"+
+	"</table>"+
+	"<div style=\"text-align: center\">"+
+	"<input type=\"button\" name=\"cmdMoveFirst\" id=\"cmdMoveFirst\" class=\"navigation\" value=\"<<\" title=\"Move First\" disabled onclick=\"navigateUserList(\'first\')\">"+
+    "<input type=\"button\" name=\"cmdMovePrevious\" id=\"cmdMovePrevious\" class=\"navigation\" value=\"<\" title=\"Move Previous\" disabled onclick=\"navigateUserList(\'previous\')\">"+
+    "<input type=\"button\" name=\"cmdMoveNext\" id=\"cmdMoveNext\" class=\"navigation\" value=\">\" title=\"Move Next\" disabled onclick=\"navigateUserList(\'next\')\">"+
+    "<input type=\"button\" name=\"cmdMoveLast\" id=\"cmdMoveLast\" class=\"navigation\" value=\">>\" title=\"Move Last\" disabled onclick=\"navigateUserList(\'last\')\">"+
+    "</div>"+
+    "<div style=\"text-align: center\">"+
+    "<input type=\"button\" name=\"cmdAddNew\" id=\"cmdAddNew\" class=\"actionButton\" value=\"Add New\" title=\"Add New\" disabled onclick=\"cmdAddNewClicked()\">"+
+    "<input type=\"button\" name=\"cmdEdit\" id=\"cmdEdit\" class=\"actionButton\" value=\"Edit\" title=\"Edit\" disabled onclick=\"cmdEditClicked()\">"+
+    "<input type=\"button\" name=\"cmdDelete\" id=\"cmdDelete\" class=\"actionButton\" value=\"Delete\" title=\"Delete\" disabled onclick=\"cmdDeleteClicked()\">"+
+    "<input type=\"button\" name=\"cmdCancel\" id=\"cmdCancel\" class=\"actionButton\" value=\"Cancel\" title=\"Cancel\" disabled onclick=\"cmdCancelClicked()\">"+
+    "<input type=\"button\" name=\"cmdSave\" id=\"cmdSave\" class=\"actionButton\" value=\"Save\" title=\"Save\" disabled onclick=\"cmdSaveClicked()\">"+
+    "</div>"+
+	"<textarea id=\"txtDatabase\" style=\"display: none;\" rows=\"1\" cols=\"1 \">"+
+	output+
+ "</textarea>"+
+"</form>";
+	
+	
+        return(output);
+}
+	%>
