13b772b38ee1bc46adff45e23ce04bcba8482489
kostjerry <kostjerry@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-11-18 18:59:54 +0000

    PollWebService implementing started ADDED: Ilsrep.PollApplication.PollWebService project

---
+++ trunk/poll/sharpteam/Ilsrep.PollApplication.PollWebService/PollWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="PollWebService.asmx.cs" Class="Ilsrep.PollApplication.PollWebService.Service1" %>
---
+++ trunk/poll/sharpteam/Ilsrep.PollApplication.PollWebService/PollWebService.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+
+namespace Ilsrep.PollApplication.PollWebService
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "Ilsrep.PollApplication.PollWebService")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class PollWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public static string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
