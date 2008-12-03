fd74d0162722dd61fe51ff0b1bed2e8d4ea7da5b
kostjerry <kostjerry@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-12-03 21:27:24 +0000

    MODIFIED: PollClient page switched to controls

--- trunk/poll/sharpteam/Web/Default.aspx.cs
+++ trunk/poll/sharpteam/Web/Default.aspx.cs
@@ -63,6 +63,107 @@ public partial class _Default : System.Web.UI.Page
                 // Save results to DB
                 break;
         }
+
+        // Left menu generating
+        leftMenuPanel.Controls.Add(new LiteralControl("<ul>"));
+        int index;
+        if ((Request["action"] == "showsurvey") || (Request["action"] == "submitpoll"))
+        {
+            LiteralControl literalControl1 = new LiteralControl();
+            literalControl1.Text = "<li><h3>" + survey.Name + ":</h3></li>";
+            leftMenuPanel.Controls.Add(literalControl1);
+            index = 1;
+            foreach (Ilsrep.PollApplication.Model.Poll curPoll in survey.Polls)
+            {
+                if (Convert.ToInt32(Session["pollIndex"]) == index - 1)
+                {
+                    LiteralControl literalControl2 = new LiteralControl();
+                    literalControl2.Text = "<li><b>" + index.ToString() + ". " + curPoll.Name + "</b></li>";
+                    leftMenuPanel.Controls.Add(literalControl2);
+                }
+                else
+                {
+                    LiteralControl literalControl3 = new LiteralControl();
+                    literalControl3.Text = "<li>" + index.ToString() + ". " + curPoll.Name + "</li>";
+                    leftMenuPanel.Controls.Add(literalControl3);
+                }
+
+                index++;
+            }
+        }
+        else
+        {
+            LiteralControl literalControl1 = new LiteralControl();
+            literalControl1.Text = "<h3>Select Survey :</h3>";
+            leftMenuPanel.Controls.Add(literalControl1);
+            index = 1;
+            foreach (Ilsrep.PollApplication.Communication.Item curItem in surveysList)
+            {
+                LiteralControl literalControl2 = new LiteralControl();
+                literalControl2.Text = "<li><a href='Default.aspx?action=showsurvey&id=" + curItem.id + "' onfocus='this.blur()'>" + index + ". " + curItem.name + "</a></li>";
+                leftMenuPanel.Controls.Add(literalControl2);
+                index++;
+            }
+        }
+        leftMenuPanel.Controls.Add(new LiteralControl("</ul>"));
+
+        // Content generating
+
+        if ((Request["action"] == "showsurvey") || (Request["action"] == "submitpoll"))
+        {
+            LiteralControl literalControl = new LiteralControl();
+            literalControl.Text = "<form class='choices' method='post' action='Default.aspx?action=submitpoll'>";
+            literalControl.Text += "<h3>" + survey.Polls[Convert.ToInt32(Session["pollIndex"])].Description + "</h3>";
+            index = 0;
+            foreach (Ilsrep.PollApplication.Model.Choice curChoice in survey.Polls[Convert.ToInt32(Session["pollIndex"])].Choices)
+            {
+                if (index == 0)
+                {
+                    literalControl.Text += "<label for='choice_" + index + "'><input checked='true' type='radio' onfocus='this.blur();' name='choice' id='choice_" + index + "' value='" + index + "' />" + curChoice.choice + "</label><br />";
+                }
+                else
+                {
+                    literalControl.Text += "<label for='choice_" + index + "'><input type='radio' onfocus='this.blur();' name='choice' id='choice_" + index + "' value='" + index + "' />" + curChoice.choice + "</label><br />";
+                }
+                index++;
+            }
+            literalControl.Text += "<input type='submit' class='submitButton' value='Continue' onfocus='this.blur();' onclick='return CheckIfSelectedChoice();' /></form>";
+            contentPanel.Controls.Add(literalControl);
+        }
+        else if (Request["action"] == "showresults")
+        {
+            LiteralControl literalControl = new LiteralControl();
+            literalControl.Text = "<div class='inner'>";
+            literalControl.Text += "<h3>Here is your Survey results:<br /></h3>";
+            float correctAnswers = 0;
+            index = 0;
+            foreach (Ilsrep.PollApplication.Model.PollResult curResult in resultsList.results)
+            {
+                index++;    
+                literalControl.Text += index + ". " + survey.Polls[curResult.questionId].Name + ": " + survey.Polls[curResult.questionId].Choices[curResult.answerId].choice + "<br />";
+                if (survey.TestMode)
+                {
+                    if (survey.Polls[curResult.questionId].Choices[curResult.answerId].Id == survey.Polls[curResult.questionId].CorrectChoiceID)
+                        correctAnswers++;
+                }
+            }
+
+            if (survey.TestMode)
+            {
+                double userScore = correctAnswers / survey.Polls.Count;
+                literalControl.Text += "<br />Your scores: " + Convert.ToInt32(userScore * 100) + "%";
+                if (userScore >= survey.MinScore)
+                {
+                    literalControl.Text += "<br /><br />Congratulations!!! You PASSED the test";
+                }
+                else
+                {
+                    literalControl.Text += "<br /><br />Sorry, try again... you FAILED";
+                }
+            }
+            literalControl.Text += "</div>";
+            contentPanel.Controls.Add(literalControl);
+        }
     }
 
     public void LogOut()
