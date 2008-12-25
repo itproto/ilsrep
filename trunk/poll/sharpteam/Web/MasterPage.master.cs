ee96704dd13aa22d669c27419bd843f5e2fcae74
vladykx <vladykx@7b32f263-214f-0410-8982-8f22a1c02a55>
2008-12-25 20:55:51 +0000

    ADDED: - Web: MasterPage UPDATED: - Web: Default.aspx: to be consistent with MasterPage

---
+++ trunk/poll/sharpteam/Web/MasterPage.master.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ilsrep.PollApplication.Communication;
+/*using Ilsrep.PollApplication.Model;
+using Ilsrep.PollApplication.Helpers;*/
+using Ilsrep.PollApplication.DAL;
+using System.Configuration;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+}
