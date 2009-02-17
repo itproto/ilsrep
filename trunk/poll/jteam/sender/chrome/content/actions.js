1b21f9ebb8a1a4ce1da116a53afb8cffdcf1dcc9
draconyster <draconyster@7b32f263-214f-0410-8982-8f22a1c02a55>
2009-02-17 14:23:43 +0000

    1) added icon to window 2) window of tabsender now appears centered 3)server name will now be customizable 4)(most important) enetered data (login pass and serverv) will be saved when user closes browser.

--- trunk/poll/jteam/sender/chrome/content/actions.js
+++ trunk/poll/jteam/sender/chrome/content/actions.js
@@ -1,6 +1,23 @@
+function loadData(){
+document.getElementById('tabsender-login').value=document.getElementById('tabsender-login').getAttribute('savedValue');
+document.getElementById('tabsender-password').value=document.getElementById('tabsender-password').getAttribute('savedValue');
+if(document.getElementById('tabsender-server').hasAttribute('savedValue')){
+document.getElementById('tabsender-server').value=document.getElementById('tabsender-server').getAttribute('savedValue');
+}
+	}
+function saveData(){
+document.getElementById('tabsender-login').setAttribute('savedValue',document.getElementById('tabsender-login').value);
+document.persist('tabsender-login','savedValue');
+document.getElementById('tabsender-password').setAttribute('savedValue',document.getElementById('tabsender-password').value);
+document.persist('tabsender-password','savedValue');
+document.getElementById('tabsender-server').setAttribute('savedValue',document.getElementById('tabsender-server').value);
+document.persist('tabsender-server','savedValue');
+	
+	}
+
 function register(){
 	var login=document.getElementById('tabsender-login').value;
-	var password=document.getElementById('tabsender-login').value;
+	var password=document.getElementById('tabsender-password').value;
 	var httpRequest = new XMLHttpRequest();
 	 httpRequest.overrideMimeType('text/xml');
 	 httpRequest.onreadystatechange = function() { registerUser(httpRequest) };
@@ -29,7 +46,7 @@ function registerUser(httpRequest){
 	}
 	function login(){
 		var login=document.getElementById('tabsender-login').value;
-	var password=document.getElementById('tabsender-login').value;
+	var password=document.getElementById('tabsender-password').value;
 	var httpRequest = new XMLHttpRequest();
 	 httpRequest.overrideMimeType('text/xml');
 	 httpRequest.onreadystatechange = function() { loginUser(httpRequest) };
