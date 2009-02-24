435ddbb126a9f3f4020d324b467ae14d99769b85
draconyster <draconyster@7b32f263-214f-0410-8982-8f22a1c02a55>
2009-02-24 16:05:16 +0000

    (by TKOST)
    IMPLEMENTED:
    - Tab Sender server logic
    - added new Tab Sender server DB actions
    
    (by DRC)
    -removed unnecessary session saving from TKOST code.
    -finished servlet
    -fixed a database bug
    The server part is now working 100%

--- trunk/poll/jteam/senderServer/src/ilsrep/sender/TabServer.java
+++ trunk/poll/jteam/senderServer/src/ilsrep/sender/TabServer.java
@@ -1,42 +1,60 @@
 package ilsrep.sender;
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServlet;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.StringWriter;
 import ilsrep.sender.protocol.Response;
+import ilsrep.sender.protocol.TabSender;
 import ilsrep.sender.db.SQLiteDBManager;
 
 public class TabServer  extends HttpServlet {
-SQLiteDBManager   db = new SQLiteDBManager("/tabsender.db");
+SQLiteDBManager   db =null;	
+	public void connectDB(){
+		String contextPath = getServletContext().getRealPath("/"); 
+String dbFilePath = contextPath + "WEB-INF/tabsender.db";
+db = new SQLiteDBManager(dbFilePath);
+		}
+
 	public void register(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 		 Response res=new Response();
          
 	   
 	        String user = (String)request.getParameter("username");
 	       String pass = (String)request.getParameter("password");
+	       String output="error";
+	          PrintWriter out = response.getWriter();
+	       
+	          try {
 	       res.setOk(db.checkUser(user).equals("true") ? false : true);
-	        if(db.checkUser(user).equals("false")) {
+	        if(!db.checkUser(user).equals("true")) {
 		                                      db.createUser(user,pass);
                }
-               String output="error";
-               try {
+               
+            
               JAXBContext responseContext = JAXBContext.newInstance(Response.class);
               Marshaller mr = responseContext.createMarshaller();
               StringWriter wr = new StringWriter();
-             mr.marshal(request, wr);
-             output=wr.toString();
+             mr.marshal(res, wr);
+            output=wr.toString();
+            
          }catch(Exception e){output=e.getMessage();};
-              PrintWriter out = response.getWriter();
+         
+           
+             
             out.write(output);        
 	                                     
                
 		}
 		
 	public void login(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+
 		 Response res=new Response();
          
 	     
@@ -48,7 +66,7 @@ SQLiteDBManager   db = new SQLiteDBManager("/tabsender.db");
               JAXBContext responseContext = JAXBContext.newInstance(Response.class);
               Marshaller mr = responseContext.createMarshaller();
               StringWriter wr = new StringWriter();
-             mr.marshal(request, wr);
+             mr.marshal(res, wr);
              output=wr.toString();
          }catch(Exception e){output=e.getMessage();};
               PrintWriter out = response.getWriter();
@@ -57,10 +75,11 @@ SQLiteDBManager   db = new SQLiteDBManager("/tabsender.db");
                
 		}
 	  public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException  {
+		  		connectDB();
              if(request.getParameter("action").equals("register")){
              register(request,response);
 	             }
-              if(request.getParameter("action").equals("register")){
+              if(request.getParameter("action").equals("login")){
              login(request,response);
 	             }  
                 
