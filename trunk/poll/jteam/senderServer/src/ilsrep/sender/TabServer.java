0890946ee184fe9f23bfa7f976c2bbed2fc58284
draconyster <draconyster@7b32f263-214f-0410-8982-8f22a1c02a55>
2009-02-25 20:10:22 +0000

    (tkost) - added methods to retrieve/store  urls - fixed bug with db connection (drac) -finished servlet entirely -the tabs are now getting stored actually on the server(so the plugin-servlet tab saving works)

--- trunk/poll/jteam/senderServer/src/ilsrep/sender/TabServer.java
+++ trunk/poll/jteam/senderServer/src/ilsrep/sender/TabServer.java
@@ -12,8 +12,11 @@ import javax.xml.bind.Unmarshaller;
 import java.io.StringWriter;
 import ilsrep.sender.protocol.Response;
 import ilsrep.sender.protocol.TabSender;
+import ilsrep.sender.protocol.Url;
 import ilsrep.sender.db.SQLiteDBManager;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.io.ByteArrayInputStream;
 public class TabServer  extends HttpServlet {
 SQLiteDBManager   db =null;	
 	public void connectDB(){
@@ -21,7 +24,65 @@ SQLiteDBManager   db =null;
 String dbFilePath = contextPath + "WEB-INF/tabsender.db";
 db = new SQLiteDBManager(dbFilePath);
 		}
-
+		
+		
+public void store(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+      PrintWriter out = response.getWriter();
+	 Response res=new Response();
+	  String user = (String)request.getParameter("username");
+	       String pass = (String)request.getParameter("password");
+	       res.setOk(db.authUser(user,pass).equals("true") ? true : false);
+	              String output="error";
+	              if(db.authUser(user,pass).equals("true")){
+		              TabSender tabsender= new TabSender();
+		                try {
+	               
+              JAXBContext urlContext = JAXBContext.newInstance(TabSender.class);
+              Unmarshaller mrs = urlContext.createUnmarshaller();
+              StringWriter wrs = new StringWriter();
+            tabsender=(TabSender)mrs.unmarshal(new ByteArrayInputStream(((String)request.getParameter("urls")).getBytes()));
+            db.storeTabs(tabsender.getUrls(),user);
+            }catch(Exception e){output=e.getMessage();out.write(output);};
+		              }
+		              
+               try {
+              JAXBContext responseContext = JAXBContext.newInstance(Response.class);
+              Marshaller mr = responseContext.createMarshaller();
+              StringWriter wr = new StringWriter();
+             mr.marshal(res, wr);
+             output=wr.toString();
+         }catch(Exception e){output=e.getMessage();};
+             
+            out.write(output);    
+	
+	}
+	public void load(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+      PrintWriter out = response.getWriter();
+	 Response res=new Response();
+	  String user = (String)request.getParameter("username");
+	       String pass = (String)request.getParameter("password");
+	       res.setOk(db.authUser(user,pass).equals("true") ? true : false);
+	        TabSender tabsender= new TabSender();
+		              tabsender.setResponse(res);
+	              String output="error";
+	              
+		                try {
+	               if(db.authUser(user,pass).equals("true")){
+		             tabsender.setUrls(db.getTabs(user));
+	             }
+              JAXBContext urlContext = JAXBContext.newInstance(TabSender.class);
+              Marshaller mrs = urlContext.createMarshaller();
+              StringWriter wrs = new StringWriter();
+            mrs.marshal(tabsender,wrs);
+            output=wrs.toString();
+            }catch(Exception e){output=e.getMessage();};
+		             
+		               
+            out.write(output);    
+	
+	}
+	
+	
 	public void register(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 		 Response res=new Response();
          
@@ -82,7 +143,12 @@ db = new SQLiteDBManager(dbFilePath);
               if(request.getParameter("action").equals("login")){
              login(request,response);
 	             }  
-                
+                if(request.getParameter("action").equals("store")){
+	                 store(request,response);
+	                }
+	                if(request.getParameter("action").equals("load")){
+	                 load(request,response);
+	                }
 }
 	
 	
