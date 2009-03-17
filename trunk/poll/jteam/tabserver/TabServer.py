fcc5bc37dfd5450bf75ecf871ff7f95db2ee1fd0
draconyster <draconyster@7b32f263-214f-0410-8982-8f22a1c02a55>
2009-03-17 16:10:19 +0000

    Server part: 1) finished the server part 2) sessions are now stored per user 3) sessions may be deleted 4) using google account as login
    Plugin part:
    1) added list of sessions that may be loaded
    2) sessions may now be deleted via special menu
    3) moved all menus to tabsender button
    4) tabs are saved per user, and a list of sessions is refreshable
    
    General:
    Deployed app to http://tabsender.appspot.com/
    
    READY FOR TESTING
    (to test simply install plugin from svn, it is configured to work with web server on google side)

--- trunk/poll/jteam/tabserver/TabServer.py
+++ trunk/poll/jteam/tabserver/TabServer.py
@@ -5,9 +5,10 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
 class Categories(db.Model):
-  user = db.UserProperty()
+  user = db.StringProperty()
   name = db.StringProperty(multiline=False)
   url = db.StringProperty(multiline=False)
+  id=db.IntegerProperty()
 
 class MainPage(webapp.RequestHandler):
   def get(self):
@@ -18,36 +19,78 @@ class MainPage(webapp.RequestHandler):
          user = users.get_current_user()
          
          if user:
-            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'true\'>'
+            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'true\' />'
          else:
-            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'false\'>'
-    
-    if action == "save":
-    	 doc = xml.dom.minidom.parse(self.request.get('urls'))
+            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'false\' />'
+    if action == "regtemp":
+         self.redirect(users.create_login_url("http://tabsender.appspot.com/html/logedin.html"))
+    if action =="remove":
          user = users.get_current_user()
          if user:
-            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'true\'>'
+            id=self.request.get('id')
+            toDelete=db.GqlQuery("select * FROM Categories where id="+id)
+            for cat in toDelete:
+                  cat.delete();
+            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'true\' />'
+         else:
+            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'false\' />'    
+    if action == "save":
+         doc = xml.dom.minidom.parseString(self.request.get('urls'))
+         doc = doc.childNodes[0]
+         user = users.get_current_user()
+         if user:
+            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'true\' />'
             name=doc.getAttribute("name")
+            categories_all = db.GqlQuery("SELECT * FROM Categories order by id desc limit 1")
+            curid=0
+            for cat in categories_all:
+               curid=cat.id+1;
             for e in doc.childNodes:
-				category= Categories()
-				category.user=user
-				category.name=name
-				category.url=e.getAttribute('link')
-				category.put()	
+                category= Categories()
+                category.user=user.nickname()
+                category.name=name
+                category.id=curid
+                category.url=e.getAttribute('link')
+                category.put()  
 
          else:
-            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'false\'>'   
+            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'false\' />'   
     if action == "load":
-    	 doc = xml.dom.minidom.Document()
-    	 user = users.get_current_user()
-    	 if user:
-            name=self.request.get("name")
-            categories_all = db.GqlQuery("SELECT * FROM Categories where name='"+name+"' and user='"+user+"'")
-			for cat in categories_all:
-
-
+         doc = xml.dom.minidom.Document()
+         urlsChild = doc.createElement("urls")
+         doc.appendChild(urlsChild)
+         urlsChild=doc.childNodes[0]
+         user = users.get_current_user()
+         if user:
+            id=self.request.get("id")
+            categories_all = db.GqlQuery("SELECT * FROM Categories where id="+id)
+            for cat in categories_all:
+               urlsChild.setAttribute('name',cat.name)
+               linkChild=doc.createElement("url")
+               linkChild.setAttribute('link',cat.url)
+               urlsChild.appendChild(linkChild)
+            output=doc.toxml()
+    
          else:
-            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'false\'>'   
+            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'false\' />'   
+    if action == "list":
+         doc = xml.dom.minidom.Document()
+         urlsChild = doc.createElement("categories")
+         doc.appendChild(urlsChild)
+         urlsChild=doc.childNodes[0]
+         user = users.get_current_user()
+         if user:
+            categories_all = db.GqlQuery("SELECT * FROM Categories where user='"+user.nickname()+"'")
+            for cat in categories_all:
+               linkChild=doc.createElement("cat")
+               linkChild.setAttribute('name',cat.name)
+               linkChild.setAttribute('id',str(cat.id))
+               urlsChild.appendChild(linkChild)
+               output=doc.toxml()
+    
+         else:
+            output='<?xml version="1.0" encoding="UTF-8"?><response isOk=\'false\' />' 
+
     self.response.out.write(output)
 
 application = webapp.WSGIApplication(
