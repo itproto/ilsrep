2c8330762bfd4c1473d88e01c43acd77537ed25c
kostjerry <kostjerry@7b32f263-214f-0410-8982-8f22a1c02a55>
2009-03-19 23:07:08 +0000

    Initial commit for kiosk application IMPLEMENTED: grid that can resize depending on user's need

---
+++ trunk/kiosk/Ilsrep.Kiosk/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ilsrep.Kiosk
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            widthTextBox.Text = kioskWindowGrid.Width.ToString();
+            heightTextBox.Text = kioskWindowGrid.Height.ToString();
+            widthSlider.Value = kioskWindowGrid.Width;
+            heightSlider.Value = kioskWindowGrid.Height;
+        }
+
+        private void widthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            kioskWindowGrid.Width = Math.Round(widthSlider.Value);
+            widthTextBox.Text = kioskWindowGrid.Width.ToString();
+        }
+
+        private void heightSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            kioskWindowGrid.Height = Math.Round(heightSlider.Value);
+            heightTextBox.Text = kioskWindowGrid.Height.ToString();
+        }
+
+        private void kioskWindowGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            mainWindow.Height = kioskWindowGrid.Height + 150;
+            mainWindow.Width = kioskWindowGrid.Width + 260;
+        }
+
+        private void widthTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                widthSlider.Value = Convert.ToDouble(widthTextBox.Text);
+                kioskWindowGrid.Width = Math.Round(widthSlider.Value);                
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void heightTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                heightSlider.Value = Convert.ToDouble(heightTextBox.Text);
+                kioskWindowGrid.Height = Math.Round(heightSlider.Value);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
