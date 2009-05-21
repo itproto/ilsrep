d1105fd0fe48582e89598078391557c90f87e461
kostjerry <kostjerry@7b32f263-214f-0410-8982-8f22a1c02a55>
2009-05-21 17:55:18 +0000

    - Model created - DAL started

---
+++ kiosk/Ilsrep.Kiosk.Protocol/Helpers/DAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace Ilsrep.Kiosk.Protocol.Helpers
+{
+    public class DAL
+    {
+        /// <summary>
+        /// Connection string specifying path to database file among other connection parameters
+        /// </summary>
+        static public string connectionString = "Data Source=\"App_Data/kiosk.db\"";
+        /// <summary>
+        /// Database connection
+        /// </summary>
+        static private SQLiteConnection dbConnection = null;
+        /// <summary>
+        /// Name of users table in database
+        /// </summary>
+        public const string USERS_TABLE = "users";
+        /// <summary>
+        /// Name of programs table in database
+        /// </summary>
+        public const string PROGRAMS_TABLE = "programs";
+
+        /// <summary>
+        /// Property that tells if database connection is active
+        /// </summary>
+        static public bool isConnected
+        {
+            get
+            {
+                return (dbConnection != null && dbConnection.State == System.Data.ConnectionState.Open);
+            }
+        }
+
+        /// <summary>
+        /// Close database connection
+        /// </summary>
+        static public void Close()
+        {
+            if (isConnected)
+                dbConnection.Close();
+        }
+
+        /// <summary>
+        /// Open database connection
+        /// </summary>
+        static private void Init()
+        {
+            dbConnection = new SQLiteConnection(connectionString);
+            dbConnection.Open();
+        }
+    }
+}
---
+++ kiosk/Ilsrep.Kiosk.Protocol/Model/User.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ilsrep.Kiosk.Protocol.Model
+{
+    public class User
+    {
+        /// <summary>
+        /// User ID in DB
+        /// </summary>
+        private int _id;
+        /// <summary>
+        /// User password(access code)
+        /// </summary>
+        private string _password;
+        /// <summary>
+        /// Number of credits available
+        /// </summary>
+        private int _credits;
+        /// <summary>
+        /// Shows is user an admin or no
+        /// </summary>
+        private bool _isAdmin;
+
+        /// <summary>
+        /// User constructor that initialize all values to 0, false or empty string
+        /// </summary>
+        public User()
+        {
+            _password = String.Empty;
+            _credits = 0;
+            _isAdmin = false;
+        }
+
+        /// <summary>
+        /// User constructor that initialize all values
+        /// </summary>
+        /// <param name="password">User password</param>
+        /// <param name="credits">User credits</param>
+        /// <param name="isAdmin">Is user an admin</param>
+        public User(string password, int credits, bool isAdmin)
+        {
+            Password = password;
+            Credits = credits;
+            IsAdmin = isAdmin;
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value != String.Empty)
+                {
+                    _password = value;
+                }
+                else
+                {
+                    throw new Exception("Password can't be empty");
+                }
+            }
+        }
+
+        public int Credits
+        {
+            get
+            {
+                return _credits;
+            }
+            set
+            {
+                _credits = value;
+            }
+        }
+
+        public bool IsAdmin
+        {
+            get
+            {
+                return _isAdmin;
+            }
+            set
+            {
+                _isAdmin = value;
+            }
+        }
+    }
+}
